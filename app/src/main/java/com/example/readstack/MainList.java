713eaa875e194cea6f3a3d66c3e2108e8db6dd5f
eprince <prince51799@gmail.com>
2022-03-03 21:57:13 -0500

    Added alerts to import export clear menu Added safteys to import export clear

--- app/src/main/java/com/example/readstack/MainList.java
+++ app/src/main/java/com/example/readstack/MainList.java
@@ -1,6 +1,8 @@
 package com.example.readstack;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +41,7 @@ public class MainList extends AppCompatActivity{
     private BookStore bookStore, newBookStore, storedStore;
     private FloatingActionButton new_button, debug_button;
     public String author, publisher, published_date, title, desc, thumbnail_link, info_link, id, caller;
+    public Boolean choice = false;
     private static final String FILE_NAME="library.json";
     private FileOutputStream fos;
     private Reader reader, exportReader, importReader;
@@ -65,6 +70,7 @@ public class MainList extends AppCompatActivity{
 
         //IF CLASS THAT CREATED THIS INTNENT ISN'T MAINACTIVITY OR BOOADDDEDDETAILS
         if(caller.equals("AddBook")) {
+            caller = "";
             newBook = createBook();
             bookDetails.add(newBook);
             try {
@@ -126,38 +132,15 @@ public class MainList extends AppCompatActivity{
         switch (item.getItemId()) {
             case R.id.menu_import:
                 Log.d("MENU", "Import Selected");
-                try{
-                    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath(), FILE_NAME);
-                    importReader = new FileReader(file.getAbsolutePath());
-                    bookStore = gson.fromJson(importReader, BookStore.class);
-                    writeToFile(gson.toJson(bookStore), MainList.this);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    importReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                recreate();
+                importAlert();
                 return true;
             case R.id.menu_export:
                 Log.d("MENU", "Export Selected");
-                try {
-                    exportReader = new FileReader(file.getAbsoluteFile());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                storedStore = gson.fromJson(exportReader, BookStore.class);
-                writeToStorage(gson.toJson(storedStore), MainList.this);
+                exportAlert();
                 return true;
             case R.id.menu_clear:
-                Log.d("MENU", "Clear Selected");
-                file = new File(getFilesDir(), FILE_NAME);
-                file.delete();
-                bookStore = null;
-                printList = null;
-                recreate();
+                clearAlert();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -177,6 +160,112 @@ public class MainList extends AppCompatActivity{
         return book;
     }
 
+    public void importAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainList.this);
+        builder.setMessage("This will override your current library");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                try{
+                    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath(), FILE_NAME);
+                    importReader = new FileReader(file.getAbsolutePath());
+                    bookStore = gson.fromJson(importReader, BookStore.class);
+                    writeToFile(gson.toJson(bookStore), MainList.this);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    importReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+                caller = "";
+                recreate();
+            }
+        });
+
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener()     {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    public void exportAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainList.this);
+        builder.setMessage("This will override previous exports");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                try {
+                    exportReader = new FileReader(file.getAbsoluteFile());
+                    try {
+                        file.delete();
+                    }
+                    catch (Exception e){
+
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                storedStore = gson.fromJson(exportReader, BookStore.class);
+                writeToStorage(gson.toJson(storedStore), MainList.this);
+                caller = "";
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener()     {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    public void clearAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainList.this);
+        builder.setMessage("This will clear your library");
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                    Log.d("MENU", "Clear Selected");
+                    file = new File(getFilesDir(), FILE_NAME);
+                    file.delete();
+                    bookStore = null;
+                    printList = null;
+                    caller = "";
+                    recreate();
+            }
+        });
+
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setNeutralButton("CANCEL", new DialogInterface.OnClickListener()     {
+            public void onClick(DialogInterface dialog, int id) {
+                //do things
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    public void setter(Boolean input){
+        choice = input;
+    }
     //WRITES JSON TO FILE_NAME
     public void writeToFile(String json, Context context){
         try {
@@ -200,8 +289,7 @@ public class MainList extends AppCompatActivity{
             outputStream.write(json.getBytes(StandardCharsets.UTF_8));
             outputStream.close();
 
-            Toast.makeText(getBaseContext(), "File exported to documents.",
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "Library.json exported to documents", Toast.LENGTH_LONG).show();
         }
         catch (Exception e) {
             e.printStackTrace();
